2da8ab765499dfa5842ec10b3fd07f5094d3f994
Erik Thompson <122100036+assinshells@users.noreply.github.com>
2026-01-02 08:25:51 +0200

    new file:   .gitignore 	new file:   backend/.env.example 	new file:   backend/.gitignore 	new file:   backend/package-lock.json 	new file:   backend/package.json 	new file:   backend/src/app.js 	new file:   backend/src/config/db.js 	new file:   backend/src/config/env.js 	new file:   backend/src/config/pino.js 	new file:   backend/src/middlewares/error.middleware.js 	new file:   backend/src/server.js 	new file:   backend/src/utils/logger.js 	new file:   frontend/.env.example 	new file:   frontend/.gitignore 	new file:   frontend/README.md 	new file:   frontend/eslint.config.js 	new file:   frontend/index.html 	new file:   frontend/package-lock.json 	new file:   frontend/package.json 	new file:   frontend/public/vite.svg 	new file:   frontend/src/app/App.css 	new file:   frontend/src/app/App.jsx 	new file:   frontend/src/assets/react.svg 	new file:   frontend/src/index.css 	new file:   frontend/src/main.jsx 	new file:   frontend/src/shared/api/axios.js 	new file:   frontend/vite.config.js 	new file:   package-lock.json 	new file:   package.json

---
+++ frontend/vite.config.js
@@ -0,0 +1,26 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@features": path.resolve(__dirname, "./src/features"),
+      "@shared": path.resolve(__dirname, "./src/shared"),
+      "@pages": path.resolve(__dirname, "./src/pages"),
+      "@app": path.resolve(__dirname, "./src/app"),
+      "@widgets": path.resolve(__dirname, "./src/widgets"),
+    },
+  },
+  server: {
+    port: 5173,
+    proxy: {
+      "/api": {
+        target: "http://localhost:5000",
+        changeOrigin: true,
+      },
+    },
+  },
+});
